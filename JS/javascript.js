1ec4ca5073f4b40a3165b68d5fb429c94097a21c
Pawel Brejnak <pawelbrejnak6@gmail.com>
2023-02-14 17:49:39 +0100

    functions update

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,52 +1,26 @@
 {
-	const hideFirstSection = () => {
-		const firstSectionButton = document.querySelector(".js-firstSectionButton")
 
-		firstSectionButton.addEventListener("click", firsButtonChangeName)
+	const hideSection = (sectionButton, section, buttonName) => {
+		sectionButton.addEventListener("click", () => {
+			section.hidden = !section.hidden
+			buttonName.innerHTML = section.hidden ? "Rozwiń sekcje" : "Zwiń sekcję"
+		})
 	}
 
-	const firsButtonChangeName = () => {
+	const init = () => {
+		const firstSectionButton = document.querySelector(".js-firstSectionButton")
 		const firstSection = document.querySelector(".js-firstSection")
 		const firstButtonName = document.querySelector(".js-firstButtonName")
-		firstSection.hidden = !firstSection.hidden
-		firstButtonName.innerHTML = firstSection.hidden
-			? "Rozwiń sekcje"
-			: "Zwiń sekcję"
-	}
-
-	const hideSecondSection = () => {
-		const secondSectionButton = document.querySelector(
-			".js-secondSectionButton"
-		)
-		secondSectionButton.addEventListener("click", secondButtonChangeName)
-	}
-
-	const secondButtonChangeName = () => {
+		const secondSectionButton = document.querySelector(".js-secondSectionButton")
 		const secondSection = document.querySelector(".js-secondSection")
 		const secondButtonName = document.querySelector(".js-secondButtonName")
-		secondSection.hidden = !secondSection.hidden
-		secondButtonName.innerHTML = secondSection.hidden
-			? "Rozwiń sekcje"
-			: "Zwiń sekcję"
-	}
-
-	const hideThirdSection = () => {
 		const thirdSectionButton = document.querySelector(".js-thirdSectionButton")
-		thirdSectionButton.addEventListener("click", thirdButtonChangeName)
-	}
-
-	const thirdButtonChangeName = () => {
 		const thirdSection = document.querySelector(".js-thirdSection")
 		const thirdButtonName = document.querySelector(".js-thirdButtonName")
-		thirdSection.hidden = !thirdSection.hidden
-		thirdButtonName.innerHTML = thirdSection.hidden
-			? "Rozwiń sekcje"
-			: "Zwiń sekcję"
-	}
-	const init = () => {
-		hideFirstSection()
-		hideSecondSection()
-		hideThirdSection()
+	
+		hideSection(firstSectionButton, firstSection, firstButtonName)
+		hideSection(secondSectionButton, secondSection, secondButtonName)
+		hideSection(thirdSectionButton, thirdSection, thirdButtonName)
 	}
 	init()
 }
